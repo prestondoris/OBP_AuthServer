8899def91063a4a562dc495e2fdf1bbd4d5ffc73
Preston Doris <prestondoris@gmail.com>
2019-06-20 12:14:05 -0700

    Continue setting up server configuration

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+This file contains all of the configuration values for the application.
+Update this file with the values for your specific Google Cloud project.
+You can create and manage projects at https://console.developers.google.com
+"""
+
+import os
+
+DATA_BACKEND = 'datastore'
+PROJECT_ID = ''
